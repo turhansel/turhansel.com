ce5042fbd654254cc7de7284b4979105c228e5f9
Turhan <turhansel@gmail.com>
2021-04-19 17:02:45 +0300

    add tilt

--- src/components/ExperienceCard/ExperienceCard.js
+++ src/components/ExperienceCard/ExperienceCard.js
@@ -1,6 +1,7 @@
 import React, { useState, createRef } from "react";
 import "./ExperienceCard.scss";
 import ColorThief from "colorthief";
+import Tilt from "react-tilt";
 
 export default function ExperienceCard({ cardInfo, isDark }) {
   const [colorArrays, setColorArrays] = useState([]);
@@ -31,57 +32,62 @@ export default function ExperienceCard({ cardInfo, isDark }) {
   };
 
   return (
-    <div className={isDark ? "experience-card-dark" : "experience-card"}>
-      <div
-        style={{ background: rgb(colorArrays) }}
-        className="experience-banner"
-      >
-        <div className="experience-blurred_div"></div>
-        <div className="experience-div-company">
-          <h5 className="experience-text-company">{cardInfo.company}</h5>
-        </div>
+    <Tilt options={{ max: 25, speed: 100, transition: true }}>
+      <div className={isDark ? "experience-card-dark" : "experience-card"}>
+        <div
+          style={{ background: rgb(colorArrays) }}
+          className="experience-banner"
+        >
+          <div className="experience-blurred_div"></div>
+          <div className="experience-div-company">
+            <h5 className="experience-text-company">{cardInfo.company}</h5>
+          </div>
 
-        <img
-          crossOrigin={"anonymous"}
-          ref={imgRef}
-          className="experience-roundedimg"
-          src={cardInfo.companylogo}
-          alt={cardInfo.company}
-          onLoad={() => getColorArrays()}
-        />
+          <img
+            crossOrigin={"anonymous"}
+            ref={imgRef}
+            className="experience-roundedimg"
+            src={cardInfo.companylogo}
+            alt={cardInfo.company}
+            onLoad={() => getColorArrays()}
+          />
+        </div>
+        <div className="experience-text-details">
+          <h5
+            className={
+              isDark
+                ? "experience-text-role dark-mode-text"
+                : "experience-text-role"
+            }
+          >
+            {cardInfo.role}
+          </h5>
+          <h5
+            className={
+              isDark
+                ? "experience-text-date dark-mode-text"
+                : "experience-text-date"
+            }
+          >
+            {cardInfo.date}
+          </h5>
+          <p
+            className={
+              isDark
+                ? "subTitle experience-text-desc dark-mode-text"
+                : "subTitle experience-text-desc"
+            }
+          >
+            {cardInfo.desc}
+          </p>
+          <ul>
+            <GetDescBullets
+              descBullets={cardInfo.descBullets}
+              isDark={isDark}
+            />
+          </ul>
+        </div>
       </div>
-      <div className="experience-text-details">
-        <h5
-          className={
-            isDark
-              ? "experience-text-role dark-mode-text"
-              : "experience-text-role"
-          }
-        >
-          {cardInfo.role}
-        </h5>
-        <h5
-          className={
-            isDark
-              ? "experience-text-date dark-mode-text"
-              : "experience-text-date"
-          }
-        >
-          {cardInfo.date}
-        </h5>
-        <p
-          className={
-            isDark
-              ? "subTitle experience-text-desc dark-mode-text"
-              : "subTitle experience-text-desc"
-          }
-        >
-          {cardInfo.desc}
-        </p>
-        <ul>
-          <GetDescBullets descBullets={cardInfo.descBullets} isDark={isDark} />
-        </ul>
-      </div>
-    </div>
+    </Tilt>
   );
 }
